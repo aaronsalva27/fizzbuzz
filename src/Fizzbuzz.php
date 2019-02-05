c1a707aefc3e10c93a99347ce300ca13e05b4571
aaronsalva27 <aaronsalva27@gmail.com>
2019-02-05 17:04:30 +0100

    unit test

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -2,8 +2,34 @@
 
 class Fizzbuzz
 {
+    const MULTIPLE_OF_THREE = 3;
+    const MULTIPLE_OF_FIVE = 5;
+
     public function convert($param)
     {
-        return 1;
+        $output = '';
+
+        if (!is_int($param)) {
+            throw new InvalidArgumentException('Argument must be an integer');
+        }
+
+        if ($param < 1) {
+            throw new InvalidArgumentException('Argument must be positive');
+        }
+
+        if($this->isMultiple($param, self::MULTIPLE_OF_THREE)) {
+            $output .= 'Fizz';
+        }
+
+        if($this->isMultiple($param, self::MULTIPLE_OF_FIVE)) {
+            $output .= 'Buzz';
+        }
+
+        return $output ? $output : $param ;
+    }
+
+    private function isMultiple($param, $num): bool
+    {
+        return !($param % $num);
     }
 }
--- tests/FizzbuzzTest.php
+++ tests/FizzbuzzTest.php
@@ -10,11 +10,89 @@ class FizzbuzzTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @test 
+     * @test
      */
     public function oneMustReturnOne()
     {
         $this->assertEquals(1, $this->sut->convert(1));
     }
+
+    /**
+     * @test
+     */
+    public function threeMustReturnFizz()
+    {
+        $this->assertEquals('Fizz', $this->sut->convert(3));
+    }
+
+    /**
+     * @test
+     */
+    public function fiveMustReturnBuzz()
+    {
+        $this->assertEquals('Buzz', $this->sut->convert(5));
+    }
+
+    /**
+     * @test
+     */
+    public function fifteenMustReturnFizzBuzz()
+    {
+        $this->assertEquals('FizzBuzz', $this->sut->convert(15));
+    }
+
+    /**
+     * @test
+     */
+    public function paramIsNegative()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sut->convert(-1);
+    }
+
+    /**
+     * @test
+     */
+    public function paramIsNotAInteger()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sut->convert('23');
+    }
+
+    /**
+     * @test
+     */
+    public function paramIsZero()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sut->convert(0);
+    }
+
+//    /**
+//     * @test
+//     */
+//    public function isMultipleOfThree()
+//    {
+//        $this->assertTrue($this->sut->isMultiple(3, 3));
+//        $this->assertFalse($this->sut->isMultiple(1, 3));
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function isMultipleOfFive()
+//    {
+//        $this->assertTrue($this->sut->isMultiple(10, 5));
+//        $this->assertFalse($this->sut->isMultiple(7, 5));
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function isMultipleOfFiveAndThree()
+//    {
+//        $this->assertTrue($this->sut->isMultiple(15, 15));
+//        $this->assertFalse($this->sut->isMultiple(20, 15));
+//    }
 
 }
